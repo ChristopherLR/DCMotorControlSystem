dc74e86d95f8d0c71a800ff4cccc92fb04516ec3
Chris <christopher.sutton12@gmail.com>
2020-06-05 01:48:16 +1000

    some images

--- OpenCalc.m
+++ OpenCalc.m
@@ -21,6 +21,8 @@ R = 2.68
 global P_motor;
 P_motor = K/((J*s + b)*(L*s + R) + K*K)
 
-linearSystemAnalyzer('step', P_motor);
+%linearSystemAnalyzer('step', P_motor);
+figure(1)
+step(P_motor);grid on;
 info = stepinfo(P_motor)
 zsyz = zpk(P_motor)
